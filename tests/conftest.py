3dfdd95be64a4d2575f2e3d3caa7e3de6c81ab58
Marilyn-Monroe <norma.jeane.mortenson@outlook.com>
2024-02-11 16:01:03 +0300

    Add secdist config in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import pathlib
+import json
 
 import pytest
 
 from testsuite.databases.pgsql import discover
+from testsuite.databases.redis import service
 
-
-pytest_plugins = ['pytest_userver.plugins.postgresql']
+pytest_plugins = [
+    'pytest_userver.plugins.postgresql',
+    'pytest_userver.plugins.redis'
+]
 
 
 @pytest.fixture(scope='session')
@@ -30,3 +34,54 @@ def pgsql_local(service_source_dir, pgsql_local_create):
         [service_source_dir.joinpath('postgresql/schemas')],
     )
     return pgsql_local_create(list(databases.values()))
+
+
+@pytest.fixture(scope='session')
+def service_env(
+        _redis_service_settings: service.ServiceSettings,
+):
+    secdist_config = {
+        'redis_settings': {
+            'redis_db_1': {
+                'password': '',
+                'sentinels': [
+                    {
+                        'host': 'redis-node-1',
+                        'port': 7000
+                    },
+                    {
+                        'host': 'redis-node-2',
+                        'port': 7001
+                    },
+                    {
+                        'host': 'redis-node-3',
+                        'port': 7002
+                    },
+                    {
+                        'host': 'redis-node-4',
+                        'port': 7003
+                    },
+                    {
+                        'host': 'redis-node-5',
+                        'port': 7004
+                    },
+                    {
+                        'host': 'redis-node-6',
+                        'port': 7005
+                    }
+                ],
+                'shards': [
+                    {
+                        'name': 'test_master0'
+                    },
+                    {
+                        'name': 'test_master1'
+                    },
+                    {
+                        'name': 'test_master2'
+                    }
+                ]
+            }
+        }
+    }
+    return {'SECDIST_CONFIG': json.dumps(secdist_config)}
